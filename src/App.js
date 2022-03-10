7a9ff21d16cf0bd8972c7e1363e5a63a43736dae
Newt-JH <flozzang@naver.com>
2022-03-10 16:49:54 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './dog.png';
 import './App.css';
+import { faker } from '@faker-js/faker';
+
+const testData = [
+  {
+    text: "냐냐냐냐냐 나는 환쨩이에용 ",
+    imgUrl: "https://img.insight.co.kr/static/2020/09/22/700/97so3hz72p4nq982if5l.jpg"
+  },
+  {
+    text: "나는 귀요미에용. ",
+    imgUrl: "https://upload.wikimedia.org/wikipedia/ko/thumb/4/4a/%EC%8B%A0%EC%A7%B1%EA%B5%AC.png/230px-%EC%8B%A0%EC%A7%B1%EA%B5%AC.png"
+  },
+  {
+    text: "누누난나~~.",
+    imgUrl: "http://image.auction.co.kr/itemimage/14/97/95/1497951b06.jpg"
+  }
+  
+  
+]
 
 function App() {
+  const h1Element = <h1>H1 제목 태그입니다.</h1>
+  const imgElement = <img src={logo} className="App-logo" alt="logo" />
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        {h1Element}
+        {imgElement}
+        
         <p>
-           <code>src/App.js</code>  귀요미 환쨩 두둥등장 ~ ~~~
+         기모륑 환쨩<code>src/App.js</code> 환쨩 귀요미
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {testData.map((contents)=>{
+           
+            return <div>
+              <img src={faker.image.avatar()} alt=""/>
+              {contents.text}
+              </div>
+          })}
+          
+        </ul>
+
       </header>
     </div>
   );
